901861992d87b73c16d76228b45eaf690670739b
RoberKrzywina <robert.krzywina@gmail.com>
2019-06-20 12:09:15 +0200

    Add config server

---
+++ config/src/main/kotlin/pl/robert/config/ConfigApplication.kt
@@ -0,0 +1,14 @@
+package pl.robert.config
+
+import org.springframework.boot.runApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+import org.springframework.cloud.config.server.EnableConfigServer
+
+@EnableConfigServer
+@SpringBootApplication
+class ConfigApplication
+
+fun main(args: Array<String>) {
+    runApplication<ConfigApplication>(*args)
+}
